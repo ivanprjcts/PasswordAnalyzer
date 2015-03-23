8e3bf37bccedf452539b63fb0a435012da8cc92f
Ivan Martin <ivanmar_91@hotmail.com>
2015-03-23 20:14:23 +0100

    add equal consecutive patterns

--- password_analyzer.py
+++ password_analyzer.py
@@ -17,6 +17,28 @@ vowels = "aeiouAEIOU"
 consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
 
 
+# patterns
+one_consecutive_vowels = '.*[' + vowels + ']{1,}.*'
+two_consecutive_vowels = '.*[' + vowels + ']{2,}.*'
+three_consecutive_vowels = '.*[' + vowels + ']{3,}.*'
+four_consecutive_vowels = '.*[' + vowels + ']{4,}.*'
+five_consecutive_vowels = '.*[' + vowels + ']{5,}.*'
+
+one_consecutive_consonants = '.*[' + consonants + ']{1,}.*'
+two_consecutive_consonants = '.*[' + consonants + ']{2,}.*'
+three_consecutive_consonants = '.*[' + consonants + ']{3,}.*'
+four_consecutive_consonants = '.*[' + consonants + ']{4,}.*'
+five_consecutive_consonants = '.*[' + consonants + ']{5,}.*'
+
+one_consecutive_equal_loweruppercase = '.*([a-zA-Z]).*'
+two_consecutive_equal_loweruppercase = '.*([a-zA-Z])\\1.*'
+three_consecutive_equal_loweruppercase = '.*([a-zA-Z])\\1\\1.*'
+four_consecutive_equal_loweruppercase = '.*([a-zA-Z])\\1\\1\\1.*'
+
+one_consecutive_numbers = '.*([0-9]).*'
+two_consecutive_numbers = '.*([0-9])\\1.*'
+three_consecutive_numbers = '.*([0-9])\\1\\1.*'
+four_consecutive_numbers = '.*([0-9])\\1\\1\\1.*'
 
 
 
@@ -97,6 +119,17 @@ def count_pattern(pattern):
 
     return counter
 
+def generate_patterns_statistic():
+    charset = lowercase_charset + uppercase_charset + numbers_charset + special_charset
+    counts = []
+    for c in charset:
+        n = char_frequency(c)
+        counts.append(n)
+        print("Frequency of " + c + ": " + str(n))
+
+    export_csv_two_equal_column("frequency_letter.csv", "Letter", charset, "Frequency", counts)
+
+
 if __name__ == '__main__':
     '''
     a_password_couter = count_passwords_contains_char('a')
@@ -108,5 +141,5 @@ if __name__ == '__main__':
     generate_all_charset_count_passwords_statistic()
     generate_all_charset_frequency_statistic()
     '''
-    res = count_pattern('.*[' + vowels + ']{3,}.*')
+    res = count_pattern(four_consecutive_numbers)
     print(res)
